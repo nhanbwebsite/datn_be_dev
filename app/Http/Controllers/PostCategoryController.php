100ca09814fcff53b1c32a865490200951330c90
hoangiang205 <hoangianglp123@gmail.com>
2022-10-03 02:06:38 +0700

    dev_giang add 3 model and controller

---
+++ app/Http/Controllers/PostCategoryController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\post_category;
+use Illuminate\Http\Request;
+
+class PostCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\post_category  $post_category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(post_category $post_category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\post_category  $post_category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, post_category $post_category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\post_category  $post_category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(post_category $post_category)
+    {
+        //
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        'id_post_category','id_user','title','short_des','content_post','image','created_by','updated_by','deleted_by'
+    ];
+    protected $table = 'post';
+}
