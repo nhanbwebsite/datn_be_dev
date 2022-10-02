c2e406c17078dd57f1ad21a6cb53d17405f74bd5
nhanbwebsite <nhanbwebsite@gmail.com>
2022-10-03 01:53:37 +0700

    s

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,34 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('/getCategoty', function (Request $request) {
+    return 'ádsadasdasd';
+});
