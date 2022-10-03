ae0fa1087a4a3d2084fbaadd7e0001278869cbc5
DuyNH1134 <92845588+duynh1134@users.noreply.github.com>
2022-10-03 10:34:30 +0700

    store user + fix

--- routes/User/user.php
+++ routes/User/user.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\UserController;
 Route::controller(UserController::class)->middleware(['auth:sanctum', 'checkAction:all,view-user,create-user,update-user,delete-user'])->prefix('users')->group(function (){
     Route::get('/', 'index');
     Route::get('/{id}', 'show');
+    Route::post('/', 'store');
+    Route::put('/{id}', 'update');
     Route::delete('/{id}', 'destroy');
 });
 ?>
