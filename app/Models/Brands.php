44df87edab07cc97d60992c5a3185442a172ae60
nhanbwebsite <nhanbwebsite@gmail.com>
2022-10-09 15:04:49 +0700

    sd

--- app/Models/Brands.php
+++ app/Models/Brands.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Brands extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'is_active',
+        'is_delete',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $attributes = [
+        'is_active' => 1,
+    ];
 }
