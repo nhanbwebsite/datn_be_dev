c0eefe28a5fb5a407bd167aac845bfffdfa966e6
nhanbwebsite <nhanbwebsite@gmail.com>
2022-10-12 22:44:23 +0700

    update route

--- routes/Categories/categories.php
+++ routes/Categories/categories.php
@@ -2,12 +2,14 @@
 use App\Http\Controllers\CategoryController;
 
 
-Route::get('/v1/categories',[CategoryController::class,'index']);
+Route::prefix('admin')->group(function(){
+    Route::get('/v1/categories',[CategoryController::class,'index']);
 
-Route::post('/v1/categories',[CategoryController::class,'store']);
+    Route::post('/v1/categories',[CategoryController::class,'store']);
 
-Route::get('/v1/categories/{id}',[CategoryController::class,'show']);
+    Route::get('/v1/categories/{id}',[CategoryController::class,'show']);
 
-Route::patch('/v1/categories/{id}',[CategoryController::class,'update']);
+    Route::patch('/v1/categories/{id}',[CategoryController::class,'update']);
 
-Route::delete('/v1/categories/{id}',[CategoryController::class,'destroy']);
+    Route::delete('/v1/categories/{id}',[CategoryController::class,'destroy']);
+});
