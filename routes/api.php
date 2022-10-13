f778644dd22d250fc2d37049e3a867601692cbdb
DuyNH1134 <92845588+duynh1134@users.noreply.github.com>
2022-10-13 09:28:52 +0700

    add comment

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Authentication
 require __DIR__ . '/Auth/auth.php';
+
+// User
 require __DIR__ . '/User/user.php';
+
+// Role
 require __DIR__ . '/Role/role.php';
+
+// GroupPermission
 require __DIR__ . '/GroupPermission/group_permission.php';
+
+// Permission
 require __DIR__ . '/Permission/permission.php';
 
 // Categories routers
