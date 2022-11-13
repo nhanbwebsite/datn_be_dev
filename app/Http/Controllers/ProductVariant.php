53e866ec36830f28be01a87f9f9c9425ab6abfc4
nhanbwebsite <nhanbwebsite@gmail.com>
2022-11-13 21:41:45 +0700

    bien the

--- app/Http/Controllers/ProductVariant.php
+++ app/Http/Controllers/ProductVariant.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Color;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
-class ColorsController extends Controller
+use App\Models\ProductVariant as ProductVariantModel;
+class ProductVariant extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -17,10 +16,11 @@ class ColorsController extends Controller
      */
     public function index()
     {
-        $data = Color::all();
+        // không phân trang
+        $data = ProductVariantModel::all();
         return response()->json([
             'status' => 'success',
-            'data'  => $data,
+            'data' => $data
         ],200);
     }
 
@@ -32,21 +32,21 @@ class ColorsController extends Controller
      */
     public function store(Request $request)
     {
-        $user = auth('sanctum')->user();
         $rules = [
             'name' => 'required',
         ];
 
         $messages = [
-            'name.required' => 'Tên màu không được để trống'
+            'name.required' => ':attribute không được bỏ trống'
         ];
 
         $attributes = [
-            'name' => 'Tên màu'
+            'name' => 'Tên biến thể'
         ];
 
         try {
             DB::beginTransaction();
+            $user = auth('sanctum')->use();
             $validator = Validator::make($request->only('name'), $rules, $messages, $attributes);
             if($validator->fails()){
                 return response()->json([
@@ -55,13 +55,11 @@ class ColorsController extends Controller
                 ], 422);
             }
 
-          $create =   Color::create([
+          $create =   ProductVariantModel::create([
                 'name' => $request->name,
                 'slug' => Str::slug($request->name),
                 'created_by' => $user->id
             ]);
-
-
             DB::commit();
         } catch(HttpException $e) {
             DB::rollBack();
@@ -86,11 +84,11 @@ class ColorsController extends Controller
     public function show($id)
     {
         try{
-            $data = Color::find($id);
+            $data = ProductVariantModel::find($id);
             if(empty($data)){
                 return response()->json([
                     'status' => 'error',
-                    'message' => 'Màu không tồn tại !',
+                    'message' => 'Biến thể không tồn tại !',
                 ], 404);
             }
         }
@@ -125,11 +123,11 @@ class ColorsController extends Controller
         ];
 
         $messages = [
-            'name.required' => 'Tên màu không được để trống'
+            'name.required' => ':attribute không được để trống !'
         ];
 
         $attributes = [
-            'name' => 'Tên màu'
+            'name' => 'Tên biến thể'
         ];
         $validator = Validator::make($request->only('name'), $rules, $messages, $attributes);
         if($validator->fails()){
@@ -140,7 +138,7 @@ class ColorsController extends Controller
         }
         try {
             DB::beginTransaction();
-            $data = Color::find($id);
+            $data = ProductVariantModel::find($id);
             $data->name = $request->name;
             $data->slug = Str::slug($request->name);
             $data->is_active = $request->is_active;
@@ -155,7 +153,6 @@ class ColorsController extends Controller
                 'line' => $e->getLine(),
             ],$e->getStatusCode()); //
         }
-
     }
 
     /**
@@ -164,17 +161,17 @@ class ColorsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id, Request $request)
+    public function destroy($id)
     {
         $user = auth('sanctum')->user();
         try {
             DB::beginTransaction();
-            $data = Color::find($id);
+            $data = ProductVariantModel::find($id);
 
             if(empty($data)){
                 return response()->json([
                     'status' => 'error',
-                    'message' => 'Không tìm thấy màu !'
+                    'message' => 'Không tìm thấy biến thể !'
                 ], 404);
             }
 
---
+++ app/Models/ProductVariant.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductVariant extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table = "ProductVariants";
+    protected $fillable = [
+        "name",
+        'is_active',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+}
