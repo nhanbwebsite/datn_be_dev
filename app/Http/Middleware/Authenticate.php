86b995014b6bd0602d1af0125586e548b15e876d
DuyNH1134 <92845588+duynh1134@users.noreply.github.com>
2022-11-14 13:55:09 +0700

    fix

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,11 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            // return route('login');
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Token không đúng'
+            ], 400);
         }
     }
 }
