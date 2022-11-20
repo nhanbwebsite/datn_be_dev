fa3209e8fb45a320f5d894d7ff83d374433a40a8
nhanbwebsite <nhanbwebsite@gmail.com>
2022-11-20 20:51:32 +0700

    s

--- routes/Products/products.php
+++ routes/Products/products.php
@@ -4,13 +4,15 @@ use App\Http\Controllers\ProductController;
 //  products routes
 
 Route::prefix('products')->middleware('auth:sanctum')->group(function (){
-    Route::get('/',[ProductController::class,'index']);
+
     Route::get('/{id}',[ProductController::class,'show']);
     Route::post('/',[ProductController::class,'store']);
     Route::put('/{id}',[ProductController::class,'update']);
     Route::delete('/{id}',[ProductController::class,'destroy']);
 });
+// public
 
+Route::get('/',[ProductController::class,'index']);
 
 Route::get('findProductByStore',[ProductController::class,'productByStore']);
 Route::get('productsBySubcate/{subId}',[ProductController::class,'producstBySubcategoryId']);
