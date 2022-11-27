2bbc002c67fe56790e2f3d721be590531b6faa0c
nhanbwebsite <nhanbwebsite@gmail.com>
2022-11-27 20:36:08 +0700

    s

--- routes/Store/store.php
+++ routes/Store/store.php
@@ -1,10 +1,11 @@
 <?php
     use App\Http\Controllers\StoreController;
 
-    Route::prefix('stores')->middleware(['auth:sanctum'])->group(function (){
-        Route::get('/',[StoreController::class,'index'])->middleware(['checkAction:all,view-store']);
+    Route::prefix('admin/stores')->middleware(['auth:sanctum'])->group(function (){
+
         Route::post('/',[StoreController::class,'store'])->middleware(['checkAction:all,create-store']);
         Route::get('/{id}',[StoreController::class,'show'])->middleware(['checkAction:all,view-store']);
         Route::put('/{id}',[StoreController::class,'update'])->middleware(['checkAction:all,update-store']);
         Route::delete('/{id}',[StoreController::class,'destroy'])->middleware(['checkAction:all,delete-store']);
     });
+    Route::get('/getStore',[StoreController::class,'index']);
