31fa8770a90be745509c7520ca77ad4607fe9389
DuyNH1134 <92845588+duynh1134@users.noreply.github.com>
2022-11-27 21:32:38 +0700

    fix

--- routes/File/file.php
+++ routes/File/file.php
@@ -3,10 +3,11 @@
 use App\Http\Controllers\FileController;
 
 Route::controller(FileController::class)->middleware('auth:sanctum')->prefix('files')->group(function(){
-    Route::get('/', 'index')->middleware('checkAction::all,view-file');
     Route::post('/', 'store')->middleware('checkAction::all,create-file');
     Route::delete('/{id}', 'destroy')->middleware('checkAction::all,delete-file');
 });
+
+Route::get('/', [FileController::class, 'index']);
 Route::get('view/{id}', [FileController::class, 'show']);
 
 ?>
