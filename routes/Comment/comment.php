1c15d7da21677f621f595dbf452933ddc2d7cb2d
nhanbwebsite <nhanbwebsite@gmail.com>
2022-12-06 21:38:29 +0700

    comment

--- routes/Comment/comment.php
+++ routes/Comment/comment.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\CommentController;
 
     Route::controller(CommentController::class)->middleware(['auth:sanctum'])->prefix('comments')->group(function (){
         Route::get('/', 'index')->middleware(['checkAction:all,view-comment']);
-        Route::post('/', 'store')->middleware(['checkAction:all,create-comment']);
+        Route::post('/', 'store');
         Route::get('/{id}', 'show')->middleware(['checkAction:all,view-comment']);
         Route::put('/{id}', 'update')->middleware(['checkAction:all,update-comment']);
         Route::delete('/{id}', 'destroy')->middleware(['checkAction:all,delete-comment']);
     });
-    Route::get('/commentsAll', [CommentController::class,'index']);
+    Route::get('/commentsAll', [CommentController::class,'index'])->middleware(['checkAction:all,view-comment']);
 ?>
