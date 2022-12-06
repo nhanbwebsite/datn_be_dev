44881ce44f2c41f6658c8ff753d52ab3d9df709d
hoangiang205 <hoangianglp123@gmail.com>
2022-12-06 09:08:07 +0700

    Giang

--- routes/FooterContent/footer-content.php
+++ routes/FooterContent/footer-content.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\FooterContentController;
         Route::get('/',[FooterContentController::class,'index']);
         Route::post('/',[FooterContentController::class,'store']);
         Route::get('/{id}',[FooterContentController::class,'show']);
-        Route::put('/{id}',[FooterContentController::class,'update']);;
+        Route::put('/{id}',[FooterContentController::class,'update']);
         Route::delete('/{id}',[FooterContentController::class,'destroy']);
     });
     Route::get('/client/footer-content/load-all',[FooterContentController::class,'loadAll']);
