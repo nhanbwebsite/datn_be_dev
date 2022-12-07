28b0170b28b041c5cfce882a21450e4ebd943dac
DuyNH1134 <92845588+duynh1134@users.noreply.github.com>
2022-12-07 22:51:56 +0700

    fix

--- app/Http/Validators/File/FileUploadValidator.php
+++ app/Http/Validators/File/FileUploadValidator.php
@@ -15,7 +15,6 @@ class FileUploadValidator extends ValidatorBase
     protected function messages(){
         return [
             'files.required' => ':attribute không được để trống !',
-            'files.file' => ':attribute phải là file !',
         ];
     }
 
