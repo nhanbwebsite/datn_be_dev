d3af6c53dbbc51c8f535cb1bb08a3ea36168083f
nhanbwebsite <nhanbwebsite@gmail.com>
2022-12-23 04:12:27 +0700

    s

--- app/Http/Controllers/ColorsController.php
+++ app/Http/Controllers/ColorsController.php
@@ -32,7 +32,7 @@ class ColorsController extends Controller
         if(isset($input['paginate'])) {
           $dataReturn =  $data->paginate($input['paginate']);
         } else{
-            $dataReturn = $data->get();
+            $dataReturn = $data->paginatte(9);
         }
 
         return response()->json([
