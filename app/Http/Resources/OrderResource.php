2be832d02b7495517fe2ce99334aeef5ec6b2336
nhanbwebsite <nhanbwebsite@gmail.com>
2022-12-23 04:43:38 +0700

    df

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -20,9 +20,9 @@ class OrderResource extends JsonResource
                 $dataDetails[$key]['product_id'] = $detail->product_id;
                 $dataDetails[$key]['product_name'] = $detail->product->name;
                 $dataDetails[$key]['variant_id'] = $detail->variant_id;
-                $dataDetails[$key]['variant_name'] = $detail->variant->variant->variant_name;
+                $dataDetails[$key]['variant_name'] = $detail->variant->variant_name;
                 $dataDetails[$key]['color_id'] = $detail->color_id;
-                $dataDetails[$key]['color_name'] = $detail->variant->pro_variant->color->name;
+                $dataDetails[$key]['color_name'] = $detail->color->name;
                 $dataDetails[$key]['product_image'] = $detail->product->url_image;
                 $dataDetails[$key]['price'] = $detail->price;
                 $dataDetails[$key]['quantity'] = $detail->quantity;
