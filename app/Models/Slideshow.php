73d87f0e010909bb232405a5f1b6f3f79ecfc50d
nhanbwebsite <nhanbwebsite@gmail.com>
2022-12-24 04:24:50 +0700

    df

--- app/Models/Slideshow.php
+++ app/Models/Slideshow.php
@@ -12,6 +12,7 @@ class Slideshow extends Model
     protected $fillable = [
         'id',
         'title',
+        'category_id',
         'is_active',
         'created_at',
         'updated_at',
