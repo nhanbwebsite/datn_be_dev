077ed6f624e5d18775472bf38b204dc6268937cd
DuyNH1134 <92845588+duynh1134@users.noreply.github.com>
2022-12-24 00:11:22 +0700

    fix

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -37,8 +37,6 @@ class OrderDetail extends Model
         return $this->belongsTo(ProductVariant::class, 'variant_id', 'id');
     }
 
-
-
     public function createdBy(){
         return $this->belongsTo(User::class, 'created_by', 'id');
     }
